0fb765c91d31ce8fb9f453d39bfdd08ac8629eb2
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2018-02-01 01:31:24 +0100

    Ajout de p_groups, le panel de ressources qui gere les groupes (WIP)

--- shmupgine-editor/p_groups.cpp
+++ shmupgine-editor/p_groups.cpp
@@ -1,6 +1,49 @@
 #include "p_groups.h"
 
-p_groups::p_groups()
-{
+p_groups::p_groups(QWidget* parent) : QWidget(parent) {
+    lv_groups       = new QListView(this);
+    model_groups    = new QStandardItemModel(this);
 
+    btn_new_group       = new QPushButton(tr("Add"), this);
+    btn_delete_group    = new QPushButton(tr("Delete"), this);
+
+    lay_mainlayout  = new QVBoxLayout(this);
+    lay_btn_group   = new QHBoxLayout(this);
+
+    lv_groups->setModel(model_groups);
+    groups_max_id = 0;
+
+    lay_btn_group->addWidget(btn_new_group);
+    lay_btn_group->addWidget(btn_delete_group);
+
+    lay_mainlayout->addWidget(lv_groups);
+    lay_mainlayout->addLayout(lay_btn_group);
+
+    connect(btn_new_group, SIGNAL(clicked(bool)), this, SLOT(new_group()));
+    connect(btn_delete_group, SIGNAL(clicked(bool)), this, SLOT(delete_group()));
+}
+
+p_groups::~p_groups() {
+
+}
+
+void p_groups::new_group() {
+    groups_max_id += 1;
+    QStandardItem* item = new QStandardItem(QString(QObject::tr("group"))+QString::number(groups_max_id));
+    item->appendRow(new QStandardItem(QString::number(groups_max_id)));
+    model_groups->appendRow(item);
+}
+
+void p_groups::delete_group() {
+    model_groups->removeRow(lv_groups->currentIndex().row());
+}
+
+
+QString p_groups::select_group() {
+    select_window *select  = new select_window(tr("Select a group"), model_groups);
+    if(select->exec()) {
+        return select->get_selected_item(-1); // name
+    } else {
+        return "";
+    }
 }
--- shmupgine-editor/p_groups.h
+++ shmupgine-editor/p_groups.h
@@ -1,11 +1,41 @@
 #ifndef P_GROUPS_H
 #define P_GROUPS_H
 
+#include <QWidget>
+#include <QListView>
+#include <QStandardItemModel>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include "singleton.h"
+#include "select_window.h"
 
-class p_groups
-{
+class p_groups : public QWidget, public Singleton<p_groups> {
+    Q_OBJECT
+
+    friend class Singleton<p_groups>;
+
 public:
-    p_groups();
+    QString select_group();
+
+public slots:
+    void new_group();
+    void delete_group();
+
+private:
+    p_groups(const p_groups&);
+    p_groups(QWidget* parent = 0);
+    virtual ~p_groups();
+
+    QListView*          lv_groups;
+    QStandardItemModel* model_groups;
+
+    QPushButton*        btn_new_group;
+    QPushButton*        btn_delete_group;
+
+    QHBoxLayout*        lay_btn_group;
+    QVBoxLayout*        lay_mainlayout;
+
+    int groups_max_id;
 };
 
 #endif // P_GROUPS_H
