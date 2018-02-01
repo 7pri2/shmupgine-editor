1dae1475f7b7210bd82cdc2abf92d910f5ac7819
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2018-02-01 01:29:49 +0100

    Creation de p_entities_editor qui est un panel qui herite de entities_attributes_panel, il se trouve dans l' editeur

--- shmupgine-editor/p_entities_editor.cpp
+++ shmupgine-editor/p_entities_editor.cpp
@@ -1,6 +1,9 @@
 #include "p_entities_editor.h"
 
-p_entities_editor::p_entities_editor()
-{
+p_entities_editor::p_entities_editor(QWidget* parent) : entities_attributes_panel(parent){
+
+}
+
+p_entities_editor::~p_entities_editor() {
 
 }
--- shmupgine-editor/p_entities_editor.h
+++ shmupgine-editor/p_entities_editor.h
@@ -1,11 +1,17 @@
 #ifndef P_ENTITIES_EDITOR_H
 #define P_ENTITIES_EDITOR_H
 
+#include "singleton.h"
+#include "entities_attributes_panel.h"
 
-class p_entities_editor
-{
-public:
-    p_entities_editor();
+class p_entities_editor : public entities_attributes_panel, public Singleton<p_entities_editor> {
+    Q_OBJECT
+
+    friend class Singleton<p_entities_editor>;
+
+private:
+    p_entities_editor(QWidget* parent = 0);
+    p_entities_editor(const p_entities_editor&);
+    virtual ~p_entities_editor();
 };
-
 #endif // P_ENTITIES_EDITOR_H
