50551b0fd73d92dbdf0c144d1729ca00a6b7d210
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2018-02-01 01:32:18 +0100

    Ajout de w_ressources, la fenetre qui reunit les panels ressources dans un tabwidget

--- shmupgine-editor/w_ressources.cpp
+++ shmupgine-editor/w_ressources.cpp
@@ -1,6 +1,44 @@
 #include "w_ressources.h"
+#include "windows_panels.h"
 
-w_ressources::w_ressources()
-{
+w_ressources::w_ressources(QWidget *parent) : QWidget(parent) {
+    /* * * * * * * * * * *
+     * MEMORY ALLOCATION *
+     * * * * * * * * * * */
 
+    tabs            = new QTabWidget(this);
+    btn_close       = new QPushButton(tr("Close"), this);
+    lay_mainlayout  = new QVBoxLayout(this);
+
+    /* * * * * *
+     * LAYOUTS *
+     * * * * * */
+
+    lay_mainlayout->addWidget(tabs);
+    lay_mainlayout->addWidget(btn_close);
+
+    /* * * * * * * * * * *
+     * WINDOW MANAGEMENT *
+     * * * * * * * * * * */
+
+    this->setWindowTitle(tr("Ressources"));
+
+    tabs->addTab(p_graphics_manager::Instance(), tr("&Graphics manager"));
+    tabs->addTab(p_entities_collection::Instance(), tr("&Entities collection"));
+    tabs->addTab(p_groups::Instance(), tr("&Groups"));
+
+    /* * * * * * * *
+     * CONNECTIONS *
+     * * * * * * * */
+
+    connect(btn_close, SIGNAL(clicked(bool)), this, SLOT(close()));
+}
+
+w_ressources::~w_ressources() {
+
+}
+
+void w_ressources::show_tab(int tab) {
+    tabs->setCurrentIndex(tab);
+    this->show();
 }
--- shmupgine-editor/w_ressources.h
+++ shmupgine-editor/w_ressources.h
@@ -1,11 +1,34 @@
 #ifndef W_RESSOURCES_H
 #define W_RESSOURCES_H
 
+#include <QWidget>
+#include <QTabWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include "singleton.h"
 
-class w_ressources
-{
-public:
-    w_ressources();
+enum {
+    graphics_manager    = 0,
+    entities_collection = 1,
+    groups              = 2
+};
+
+class w_ressources : public QWidget, public Singleton<w_ressources> {
+    Q_OBJECT
+
+    friend class Singleton<w_ressources>;
+
+public slots:
+    void show_tab(int tab);
+
+private:
+    w_ressources(const w_ressources&);
+    w_ressources(QWidget* parent = 0);
+    ~w_ressources();
+
+    QTabWidget*     tabs;
+    QPushButton*    btn_close;
+    QVBoxLayout*    lay_mainlayout;
 };
 
 #endif // W_RESSOURCES_H
