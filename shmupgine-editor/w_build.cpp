2747da7be2e85b7710ff7e216bf44a5db873f3eb
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2018-06-09 02:50:48 +0200

    Creation de la fenetre de build, qui montrera l'avancement de la contruction du jeu

--- shmupgine-editor/w_build.cpp
+++ shmupgine-editor/w_build.cpp
@@ -1,6 +1,39 @@
 #include "w_build.h"
 
-w_build::w_build(QWidget *parent) : QWidget(parent)
-{
+w_build::w_build(QWidget *parent) : QWidget(parent) {
+    /* * * * * * * * * * *
+     * MEMORY ALLOCATION *
+     * * * * * * * * * * */
 
+    lay_main = new QVBoxLayout(this);
+    btn_close = new QPushButton(tr("Close"), this);
+    pb_compilation_progress = new QProgressBar(this);
+
+    /* * * * * *
+     * LAYOUTS *
+     * * * * * */
+
+    lay_main->addWidget(pb_compilation_progress);
+    lay_main->addWidget(btn_close);
+
+    /* * * * * * * * * * *
+     * WINDOW MANAGEMENT *
+     * * * * * * * * * * */
+
+    pb_compilation_progress->setMaximum(10);
+    pb_compilation_progress->setValue(4);
+    this->setProperty("class", "background");
+
+    /* * * * * * * *
+     * CONNECTIONS *
+     * * * * * * * */
+
+    connect(btn_close, SIGNAL(clicked(bool)), this, SLOT(close()));
+}
+
+w_build::~w_build() {
+
+}
+
+void w_build::build() {
 }
--- shmupgine-editor/w_build.h
+++ shmupgine-editor/w_build.h
@@ -2,16 +2,29 @@
 #define W_BUILD_H
 
 #include <QWidget>
+#include <QProgressBar>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QVariant>
+#include "singleton.h"
 
-class w_build : public QWidget
-{
+class w_build : public QWidget, public Singleton<w_build> {
     Q_OBJECT
-public:
-    explicit w_build(QWidget *parent = 0);
 
-signals:
+    friend class Singleton<w_build>;
 
 public slots:
+    void build();
+
+private:
+    w_build(const w_build&);
+    w_build(QWidget *parent = 0);
+    ~w_build();
+
+    QProgressBar*   pb_compilation_progress;
+    QPushButton*    btn_close;
+    QVBoxLayout*    lay_main;
 };
 
 #endif // W_BUILD_H
