baca709dfb69914a8f45a8151b5ae804431690b4
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2018-06-09 02:49:58 +0200

    Amelioration du nightmode: boutons restylises

---
+++ shmupgine-editor/w_build.cpp
@@ -0,0 +1,6 @@
+#include "w_build.h"
+
+w_build::w_build(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ shmupgine-editor/w_build.h
@@ -0,0 +1,17 @@
+#ifndef W_BUILD_H
+#define W_BUILD_H
+
+#include <QWidget>
+
+class w_build : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit w_build(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // W_BUILD_H
